22d2e001bb7542cf736f100e95feb974b078e623
Ash-Cozey <62861333+Ash-Cozey@users.noreply.github.com>
2024-05-02 16:59:09 -0600

    changed default config

--- keyboards/thumbsup/rev7_rp2040_split_5x12/config.h
+++ keyboards/thumbsup/rev7_rp2040_split_5x12/config.h
@@ -1,26 +1,26 @@
 /*
  * Copyright 2023 Alexander (Sasha) Karmanov <thumbsup.shop@karmanov.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
 
-#define SPLIT_TRANSPORT_MIRROR
-#define SPLIT_LAYER_STATE_ENABLE
+//#define SPLIT_TRANSPORT_MIRROR
+//#define SPLIT_LAYER_STATE_ENABLE
 //#define SPLIT_LED_STATE_ENABLE
-#define SPLIT_MODS_ENABLE
+//#define SPLIT_MODS_ENABLE
 
 //#undef I2C_DRIVER
 //#define I2C_DRIVER I2CD1
@@ -29,7 +29,7 @@
 //#undef I2C1_SDA_PIN
 //#define I2C1_SDA_PIN GP24
 
-//#define EE_HANDS
+#define EE_HANDS
 /* #define MASTER_LEFT */
 /* #define MASTER_RIGHT */
 /* #define SPLIT_HAND_PIN GP18*/
@@ -51,13 +51,13 @@
 //#define MOUSEKEY_TIME_TO_MAX       7 //20
 //#define MOUSEKEY_WHEEL_MAX_SPEED   5	//8
 //#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
- 
 
-//#define USB_POLLING_INTERVAL_MS 1
+
+#define USB_POLLING_INTERVAL_MS 1
 
 
 //The number of layers for VIA/REMAP
-//#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 
 //This disables the auto-repeat for the double-action keys (e.g. S+LAlt)
 //#define QUICK_TAP_TERM 0
